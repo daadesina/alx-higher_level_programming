5c8f2cdb06a7afc56f04ed359193106ca7999987
daadesina <daadesina1@gmail.com>
2024-02-03 04:49:14 +0100

    adesina

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module of function"""
+
+
+def add_integer(a, b=98):
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not isinstance(a, int):
+        raise TypeError('a must be an integer')
+    elif not isinstance(b, int):
+        raise TypeError('b must be an integer')
+    else:
+        return (a + b)
