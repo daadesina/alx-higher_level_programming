24847d4792fd7db1e57247b4bb320caeaf8a45ba
daadesina <daadesina1@gmail.com>
2024-02-04 10:47:25 +0100

    adesina

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""A module of function"""
+
+
+def print_square(size):
+    """A function that prints a square"""
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
