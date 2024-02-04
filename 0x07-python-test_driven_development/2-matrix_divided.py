f42c805d36d445f49d6bdaabc047a5173c66d277
daadesina <daadesina1@gmail.com>
2024-02-04 08:38:30 +0100

    adesina

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,6 +11,14 @@ def matrix_divided(matrix, div):
 
     new_mat = [[0 for i in range(col)] for j in range(row)]
 
+    for i in range(row - 1):
+        if len(matrix[i]) == len(matrix[i + 1]):
+            pass
+        else:
+            raise TypeError('Each row of the matrix must have the same size')
+
+
+    
     for i in range(row):
         for j in range(col):
             if isinstance(matrix[i][j], int):
@@ -19,5 +27,8 @@ def matrix_divided(matrix, div):
                 new_mat[i][j] = round((matrix[i][j] / div), 2)
             else:
                 raise TypeError('matrix must be a matrix of integers/floats')
+    
+
+
 
     return (new_mat)
