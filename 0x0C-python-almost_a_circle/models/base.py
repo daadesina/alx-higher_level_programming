6eb39ffcf1323538428e6a44149089dbdd697c3a
daadesina <daadesina1@gmail.com>
2024-02-11 18:51:57 +0100

    adesina

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def validator(self, name, *argv):
+        for i in argv:
+            if type(argv) is not int:
+                raise TypeError(f'{name} must be an integer')
+
+            if i <= 0:
+                raise ValueError(f'{name} must be > 0')
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,41 +7,91 @@ from models.base import Base
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''self.__width = width
+        self.__width = width
         self.__height = height
         self.__x = x
-        self.__y = y'''
+        self.__y = y
 
         super().__init__(id)
+
+
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        elif type(height) is not int:
+            raise TypeError('height must be an integer')
+        elif type(x) is not int:
+            raise TypeError('x must be an integer')
+        elif type(y) is not int:
+            raise TypeError('y must be an integer')
+
+        if width <= 0:
+            raise ValueError('width must be > 0')
+        if height <= 0:
+            raise ValueError('height must be > 0')
+        elif x < 0:
+            raise ValueError('x must be >= 0')
+        elif y < 0:
+            raise ValueError('y must be >= 0')
+
 
     @property
     def width(self):
         return (self.__width)
 
     @width.setter
-    def width(self):
-        self.__width = width
+    def width(self, value):
+
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
 
     @property
     def height(self):
         return (self.__height)
     @height.setter
-    def height(self):
-        self.__height = height
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        
+        self.__height = value
 
     @property
     def x(self):
         return (self.__x)
 
     @x.setter
-    def x(self):
-        self.__x = x
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        
+        self.__x = value
 
     @property
     def y(self):
         return (self.__y)
 
     @y.setter
-    def y(self):
-        self.__y = y
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
 
+        self.__y = value
+
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def display(self):
+        for i in range(self.__height - 1):
+            for j in range(self.__width):
+                print('#', end="")
+            print()
+        print(self.__width * '#')
