8f04a2a3e321807aa3a24191c4c755d7e92a88c6
daadesina <daadesina1@gmail.com>
2024-03-16 11:16:27 +0100

    adesina

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,3 @@
--- Creates a table called first_table with values id and name.
+-- Write a script that creates a table called first_table in the current database in your MySQL server.
+
 CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
