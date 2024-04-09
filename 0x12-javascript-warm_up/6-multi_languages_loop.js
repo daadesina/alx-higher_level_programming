9c77a0ebf252f5c0416536ef646deaf70b86d44d
daadesina <daadesina1@gmail.com>
2024-04-09 21:59:47 +0100

    adesina

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let my_array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let num =  0;
+
+while (num < my_array.length)
+{
+	console.log(my_array[num]);
+	num++;
+}
