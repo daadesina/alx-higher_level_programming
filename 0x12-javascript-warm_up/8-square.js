faf4b19d8279dedf34643861c07429f87983e3b4
daadesina <daadesina1@gmail.com>
2024-04-10 14:28:43 +0100

    adesina

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,10 +5,17 @@ const argv = require('process');
 let num = process.argv[2];
 let count = process.argv[2];
 
-while (count > 0)
+if (isNaN(num))
 {
-	console.log('X'.repeat(num));
-	count--;
+	console.log('Missing size');
+}
+else
+{
+	while (count > 0)
+	{
+		console.log('X'.repeat(num));
+		count--;
+	}
 }
 
 
