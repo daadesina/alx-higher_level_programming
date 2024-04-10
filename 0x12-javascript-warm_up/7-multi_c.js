53e40669f6d37f8b39ad3a202041eca945439c3d
daadesina <daadesina1@gmail.com>
2024-04-10 14:33:19 +0100

    adesina

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const argv = require('process');
+
+let count = process.argv[2];
+
+if (isNaN(count))
+{
+	console.log('Missing number of occurrences');
+}
+else
+{
+	while (count > 0)
+	{
+		console.log('C is fun');
+		count--;
+	}
+}
+
+
